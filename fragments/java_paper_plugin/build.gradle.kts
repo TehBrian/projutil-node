2aef42c2e3b80a179374b5c89006e73523c57ce8
TehBrian <tehbrian@protonmail.com>
2022-01-13 16:13:03 -0500

    java_paper_plugin: remove unused sonatype repos

--- fragments/java_paper_plugin/build.gradle.kts
+++ fragments/java_paper_plugin/build.gradle.kts
@@ -16,12 +16,6 @@ repositories {
     maven("https://papermc.io/repo/repository/maven-public/") {
         name = "papermc"
     }
-    maven("https://oss.sonatype.org/content/groups/public/") {
-        name = "sonatype"
-    }
-    maven("https://s01.oss.sonatype.org/content/groups/public/") {
-        name = "sonatype-s01"
-    }
 }
 
 dependencies {
